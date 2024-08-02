c1444400741331008920c3abc4b8517a028f470b
Pedro Werkhaizer <pedro.werkhaizer@gmail.com>
2024-08-01 21:10:13 -0300

    Initial Commit

---
+++ Memory Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memory Game
+//
+//  Created by Pedro Werkhaizer on 01/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memory Game/Memory_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_GameApp.swift
+//  Memory Game
+//
+//  Created by Pedro Werkhaizer on 01/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Memory_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
